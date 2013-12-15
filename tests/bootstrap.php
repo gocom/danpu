d10e3ec33e06ea1f2c2abd84596c995390e086fc
Jukka Svahn <web@rahinaa.biz>
2013-12-16 01:03:23 +0200

    Check for notices.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,6 +4,11 @@ error_reporting(E_ALL);
 
 include dirname(__DIR__) . '/vendor/autoload.php';
 
+set_error_handler(function ($errno, $errstr, $errfile, $errline)
+{
+    throw new Exception($errstr . ' in ' . $errfile . ' on line ' . $errline);
+});
+
 $loader = new \Composer\Autoload\ClassLoader();
 $loader->add('Rah\Danpu\Test', __DIR__);
 $loader->register();
