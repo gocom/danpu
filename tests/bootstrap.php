a4716145216655ed6144c25a049f36564b8649fc
Jukka Svahn <void@rah.pw>
2018-11-30 21:45:10 +0200

    Use autoload-dev for tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,4 @@
 <?php
 
 error_reporting(E_ALL);
-
-include dirname(__DIR__) . '/vendor/autoload.php';
-
-$loader = new \Composer\Autoload\ClassLoader();
-$loader->add('Rah\Danpu\Test', __DIR__);
-$loader->register();
+require dirname(__DIR__) . '/vendor/autoload.php';
